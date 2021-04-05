e045d2d53b6f550eb7a1ed8b3aa4cc24dd662344
Sumya Akter <18101029@uap-bd.edu>
2021-04-06 01:37:11 +0600

    new data

--- create_data.py
+++ create_data.py
@@ -12,7 +12,7 @@ datasets = 'datasets'
 # These are sub data sets of folder, 
 # for my faces I've used my name you can 
 # change the label here
-sub_data = 'rumman'     
+sub_data = 'Sumya'     
   
 path = os.path.join(datasets, sub_data)
 if not os.path.isdir(path):
